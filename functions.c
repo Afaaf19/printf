188ca56345941072e72a1cc307c9dfa6479d60e4
Afaaf19 <afaf.mourky@gmail.com>
2023-05-30 16:21:52 -0700

    some changes

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
+ * print_char - function that Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -15,9 +15,9 @@
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
+	char ch = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(ch, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
 /**
@@ -34,7 +34,7 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int length = 0, i;
-	char *str = va_arg(types, char *);
+	char *s = va_arg(types, char *);
 
 	UNUSED(buffer);
 	UNUSED(flags);
@@ -43,12 +43,12 @@ int print_string(va_list types, char buffer[],
 	UNUSED(size);
 	if (str == NULL)
 	{
-		str = "(null)";
+		s = "(null)";
 		if (precision >= 6)
-			str = "      ";
+			s = "      ";
 	}
 
-	while (str[length] != '\0')
+	while (s[length] != '\0')
 		length++;
 
 	if (precision >= 0 && precision < length)
@@ -67,12 +67,12 @@ int print_string(va_list types, char buffer[],
 		{
 			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
-			write(1, &str[0], length);
+			write(1, &s[0], length);
 			return (width);
 		}
 	}
 
-	return (write(1, str, length));
+	return (write(1, s, length));
 }
 /************************* PRINT PERCENT SIGN *************************/
 /**
